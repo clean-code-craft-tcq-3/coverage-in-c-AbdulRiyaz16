4eb250c1531fd1e5118711d2bd88d590e5af9abd
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-27 02:46:37 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -17,7 +17,7 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
 {
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
   breachType == TOO_HIGH ? printf("\ntemperature is too high\n") : breachType == TOO_LOW ? printf("\ntemperature is too low\n"): printf("\ntemperature is Normal\n");
-  
+  checkAlertTarget(alertTarget,breachType);
 }
   
 
