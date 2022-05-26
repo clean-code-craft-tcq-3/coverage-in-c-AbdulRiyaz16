7d634bacb07e14912d155bf8fd8264651cbd4ba3
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-27 03:36:37 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -12,3 +12,27 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(105,100,105) == TOO_HIGH);
   REQUIRE(inferBreach(120,100,105) == TOO_HIGH);
 }
+
+TEST_CASE("Check for checkAndAlert functionality") {
+   BatteryCharacter batteryProp1 = {PASSIVE_COOLING, "ABCD"};
+  BatteryCharacter batteryProp2 = {HI_ACTIVE_COOLING, "BCDE"};
+  BatteryCharacter batteryProp3 = {MED_ACTIVE_COOLING, "PQRS"};
+  checkAndAlert(TO_CONTROLLER, batteryProp1, 20.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20.0)==NORMAL);
+  checkAndAlert(TO_EMAIL, batteryProp1, -5.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5.0)==TOO_LOW);
+  checkAndAlert(TO_CONTROLLER, batteryProp1, 70.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 70.0)==TOO_HIGH);
+  checkAndAlert(TO_CONTROLLER, batteryProp2, 22.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 22.0)==NORMAL);
+  checkAndAlert(TO_EMAIL, batteryProp2, -6.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -6.0)==TOO_LOW);
+  checkAndAlert(TO_CONTROLLER, batteryProp2, 80.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 80.0)==TOO_HIGH);
+  checkAndAlert(TO_CONTROLLER, batteryProp3, 20.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20.0)==NORMAL);
+  checkAndAlert(TO_EMAIL, batteryProp3, -9.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -9.0)==TOO_LOW);
+  checkAndAlert(TO_CONTROLLER, batteryProp3, 90.0);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 90)==TOO_HIGH);
+}
