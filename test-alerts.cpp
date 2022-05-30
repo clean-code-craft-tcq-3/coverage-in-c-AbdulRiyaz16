98aa2ac58f2cb7e1cd8bb2762e7e4d6fc3323b2f
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-30 09:57:30 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,7 +8,6 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(-12, 0, 35) == TOO_LOW);
   REQUIRE(inferBreach(101,100,105) == NORMAL);
   REQUIRE(inferBreach(25,20,30)==NORMAL);
-  REQUIRE(inferBreach(105,100,105) == TOO_HIGH);
   REQUIRE(inferBreach(120,100,105) == TOO_HIGH);
 }
 
